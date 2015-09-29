ebdbeb8dedc3129b345a1f7ec84e602383a8fb05
Roopesh Reddy <cr.reddy@hotmail.com>
2015-09-29 11:44:19 +0530

    Performance tweaks

--- InfiniteScroll.js
+++ InfiniteScroll.js
@@ -1,4 +1,4 @@
-/*global appendItemsToList,removeItemsFromList,prependItemsToList,chunk,createListItems*/
+/*global appendItemsToList,removeItemsFromList,prependItemsToList,chunk,createListItems,requestAnimationFrame*/
 /*jslint nomen:true,plusplus:true*/
 
 
@@ -26,9 +26,11 @@ function InfiniteScroll(options) {
                 throw new Error("No items or root element is not set!");
             }
         
+            /*
             document.getElementById(options.wrapper).addEventListener("scroll", function () {
                 position = this.scrollTop / (this.scrollHeight - this.clientHeight);
             }, false);
+            */
             
             index = -1;
             
@@ -40,18 +42,27 @@ function InfiniteScroll(options) {
                 respective events!
             
             */
+           
             
+            var wrapper = document.getElementById(options.wrapper);
+            
+           
             (function initScroll() {
-            
                 watchScrolling = setTimeout(function () {
-
+                    
+                    position = wrapper.scrollTop / (wrapper.scrollHeight - wrapper.clientHeight);
+                    
+                                       
                     if (prevPosition <= position) {
 
                         if (position > 0.9 && index < items.length && (index + 1) !== items.length) {
 
                             removeItemsFromList(index);
                             appendItemsToList(++index);
-                            document.getElementById(options.wrapper).scrollTop = document.querySelectorAll("#" + options.wrapper + ">ul>.item" + (index - 1))[0].clientHeight * 70;
+                            
+                            document.getElementById(options.wrapper).scrollTop = 
+                                document.querySelectorAll("#" + options.wrapper + ">ul>.item" + (index - 1))[0].clientHeight * 70;
+                            
                         }
 
                     } else {
@@ -61,17 +72,21 @@ function InfiniteScroll(options) {
                             removeItemsFromList(index + 1);
                             --index;
                             prependItemsToList(index - 1);
-                            document.getElementById(options.wrapper).scrollTop = document.querySelectorAll("#" + options.wrapper + ">ul>.item" + (index - 1))[0].clientHeight * 100;
+                            document.getElementById(options.wrapper).scrollTop = 
+                                document.querySelectorAll("#" + options.wrapper + ">ul>.item" + (index - 1))[0].clientHeight * 100;
+                           
+                            
                         }
                     }
                     prevPosition = position;
-
+                    
                     initScroll();
 
-                }, 750);
-                
+                },250);
+               
             }());
-                
+            
+            
         },
         
         chunk = function () {
@@ -102,6 +117,7 @@ function InfiniteScroll(options) {
                 elements = document.querySelectorAll("#" + options.wrapper + ">ul>.item" + removeIndex);
                 for (i = 0; i < elements.length; i = i + 1) {
                     elements[i].parentNode.removeChild(elements[i]);
+                    
                 }
 
             }
@@ -109,15 +125,18 @@ function InfiniteScroll(options) {
         
         prependItemsToList = function (chunkIndex) {
             var docFragment = createListItems(chunkIndex);
-            document.querySelector("#" + options.wrapper + ">ul").insertBefore(docFragment, document.querySelectorAll("#" + options.wrapper + ">ul>.item" + (chunkIndex + 1))[0]);
+            document.querySelector("#" + options.wrapper + ">ul").insertBefore(docFragment, 
+                    document.querySelectorAll("#" + options.wrapper + ">ul>.item" + (chunkIndex + 1))[0]);
         },
         
         createListItems = function (chunkIndex) {
+                        
             var i = 0,
                 itemsCount = items[chunkIndex].length,
                 elem = null,
                 docFragment = document.createDocumentFragment();
 
+            
             for (i = 0; i < itemsCount; i = i + 1) {
                 elem = items[chunkIndex][i];
                 elem.setAttribute('class', "item" + chunkIndex);
